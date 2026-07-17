b12de60580befd4b7628124eda84678c75d25053
Rabbani955 <srabbani955@gmail.com>
2026-07-17 16:03:53 +0530

    navigation for availability

--- src/App.jsx
+++ src/App.jsx
@@ -1816,7 +1816,8 @@ function HomeView({ rooms, onSelectRoom, occupiedRooms }) {
         <div
           id="rooms"
           ref={roomsSectionRef}
-          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20 bg-slate-200"
+
+          className="max-w-screen-2xl mx-auto px-6 py-20 bg-slate-200"
         >
           <div className="text-center mb-16">
             <h2 className="text-4xl font-extrabold text-slate-900 mb-4 tracking-tight">
